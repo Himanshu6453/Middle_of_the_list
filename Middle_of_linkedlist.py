7bf10547f44ff3db31274dc74d51ecfe15bd6dee
Himanshu <71595502+Himanshu6453@users.noreply.github.com>
2022-09-26 11:33:55 +0530

    Create Middle_of_linkedlist.py

---
+++ Middle_of_linkedlist.py
@@ -0,0 +1,19 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    def middleNode(self, head):
+        list_len = 0
+        start=nod=head
+        while start:
+            list_len += 1
+            start = start.next
+        center = list_len // 2
+        count = 0
+        while nod:
+            if count == center:
+                return nod
+            else:
+                count+=1
+                nod = nod.next
